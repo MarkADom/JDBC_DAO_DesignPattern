b563d45817668fed096e2a9562ecc423369aa7ff
Marco Domingues <marco.a.dom78@gmail.com>
2023-01-20 18:30:05 +0000

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,7 +26,7 @@ public class Program {
         System.out.println(seller);
 
         System.out.println("\n==== TEST 2: seller findByDepartment ====");
-        Department department = new Department(2,null);
+        Department department = new Department(2, null);
 
         List<Seller> list = sellerDao.findByDepartment(department);
 
@@ -41,5 +41,11 @@ public class Program {
             System.out.println(obj);
         }
 
+
+        System.out.println("\n==== TEST 4: seller insert ====");
+
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id =  " +newSeller.getId());
     }
 }
