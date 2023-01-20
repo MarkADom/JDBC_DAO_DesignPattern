e5e24f1e9c52de47805849b45921394b91593e7e
Marco Domingues <marco.a.dom78@gmail.com>
2023-01-20 20:34:09 +0000

    resouces folder added

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,7 @@ public class Program {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         /*
            Calling Daofactory,that way my program doesn't know the implementation.
            He only knows the interface and also a way for us to do a dependency
@@ -20,31 +21,25 @@ public class Program {
          */
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
         System.out.println("==== TEST 1: seller findById ====");
-
         Seller seller = sellerDao.findByID(3);
         System.out.println(seller);
 
         System.out.println("\n==== TEST 2: seller findByDepartment ====");
         Department department = new Department(2, null);
-
         List<Seller> list = sellerDao.findByDepartment(department);
-
         for (Seller obj : list) {
             System.out.println(obj);
         }
 
         System.out.println("\n==== TEST 3: seller findByAll ====");
         list = sellerDao.findAll();
-
         for (Seller obj : list) {
             System.out.println(obj);
         }
 
 
         System.out.println("\n==== TEST 4: seller insert ====");
-
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New Id =  " +newSeller.getId());
@@ -61,7 +56,6 @@ public class Program {
         int id = sc.nextInt();
         sellerDao.deleteById(id);
         System.out.println("Delete completed!");
-
 
         sc.close();
     }
