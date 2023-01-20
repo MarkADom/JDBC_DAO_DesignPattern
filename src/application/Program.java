7a3cefe3cdb4b0276e8b31fd329f4c83e9659677
Marco Domingues <marco.a.dom78@gmail.com>
2023-01-20 12:03:52 +0000

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,12 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n==== TEST 3: seller findByAll ====");
+        list = sellerDao.findAll();
+
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -99,7 +99,43 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+
+
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "ORDER BY Name");
+
+            //executeQuery(), executes the above SQL command, and it's received by ResultSet.
+            rs = st.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                //Testing if the department exist
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    //Inserting department on Map
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
